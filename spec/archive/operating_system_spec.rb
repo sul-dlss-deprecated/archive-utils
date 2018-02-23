a7550753fe8682c9299714c446fc12444f20a199
Naomi Dushay <ndushay@stanford.edu>
2018-02-23 00:07:32 -0800

    operating_system_spec: remove crufty comments from yucky spec generator and modernize

--- spec/archive/operating_system_spec.rb
+++ spec/archive/operating_system_spec.rb
@@ -1,12 +1,8 @@
-require_relative '../spec_helper'
-
 describe 'Archive::OperatingSystem' do
 
-  # For input parameters:
-  # * command [String] = the command to be executed
-  specify 'Archive::OperatingSystem.execute' do
+  it 'Archive::OperatingSystem.execute' do
     command = 'echo "hello world!"'
-    expect(Archive::OperatingSystem.execute(command)).to eq("hello world!\n")
+    expect(Archive::OperatingSystem.execute(command)).to eq "hello world!\n"
     command = 'non-existent command'
     expect{Archive::OperatingSystem.execute(command)}.to raise_error(/Command failed to execute/)
   end
